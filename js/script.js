178526ba6d89df4a02116faf656a0accbb9818ee
Aldo Cahuana <aldooow@icloud.com>
2020-05-20 18:26:46 +0200

    Work in progress!!!!!

--- js/script.js
+++ js/script.js
@@ -7,31 +7,30 @@ var eta = prompt('Qual è la tua età??');
 // Il costo del Biglietto per ogni Km!!!!!
 var prezzo = 0.21 * kilometri;
 
-// Degli sconti sulle fasce di età!!!!
-var sconto20 = prezzo - ((20 * prezzo) / 100);
-var sconto40 = prezzo - ((40 * prezzo) / 100);
-
 // variante Prezzo Finale!!!
 var prezzoFinale;
 
 // Autenticare l'uttilizzo di soltanto numeri nel Form!!!!
-if (isNaN(kilometri) || isNaN(eta)) {
-  alert('Questo form accetta soltanto numeri, in una delle opzioni hai inserito delle lettere!!!');
-} else {
-
+if (!isNaN(kilometri) || !isNaN(eta)) {
   // Prezzo Finale!!
-  if (eta <= 18) {
+  if ((eta <= 18) && (eta > 0)) {
     alert('Hai ottenuto uno sconto del 20%!!');
-    (prezzoFinale = sconto20);
-  } else if (eta >=65) {
+    (prezzoFinale = prezzo - ((20 * prezzo) / 100));
+  } else if (eta >= 65) {
     alert('Hai ottenuto uno sconto del 40%!!');
-    (prezzoFinale = sconto40);
+    (prezzoFinale = prezzo - ((40 * prezzo) / 100));
+  } else if (eta <= 0) {
+    alert('Non è permesso il numero 0 come età!!!');
+    (prezzoFinale = 'NO ');
   } else {
     (prezzoFinale = prezzo);
   }
+} else {
+  alert('Questo form accetta soltanto numeri, in una delle opzioni hai inserito delle lettere!!!');
 }
 
-var risultato = 'Il ' +  'prezzo ' +  'del ' + 'biglietto ' + 'per ' + 'te ' + 'è ' +  prezzoFinale;
+// Risultato!!!!
+var risultato = 'Il ' +  'prezzo ' +  'del ' + 'biglietto ' + 'per ' + 'te ' + 'è ' +  prezzoFinale + 'eur.';
 
 
 document.getElementById('prezzo').innerHTML = risultato;
